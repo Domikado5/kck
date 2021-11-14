37a3da80e9195889fcdbb5ba85105478013c9868
D3nz13 <seba.szczepan2@gmail.com>
2021-11-14 18:10:08 +0100

    Fixed image transformations

--- lab5 - images project/dice_recognition.py
+++ lab5 - images project/dice_recognition.py
@@ -7,9 +7,9 @@ def null_func(arg):
 
 
 def draw_circles(image, circles):
-    circles_np = np.uint16(np.around(circles))
-    for i in circles_np[0,:]:
-        cv2.circle(image, (i[0], i[1]), i[2], (255, 0, 0), 2)
+    circles_np = np.round(circles[0, :]).astype('int')
+    for (x, y, radius) in circles_np:
+        cv2.circle(image, (x, y), radius, (255, 0, 0), 2)
 
 
 def main():
@@ -27,13 +27,17 @@ def main():
     cv2.createTrackbar('hough_toggle', 'window', 0, 1, null_func)
     cv2.createTrackbar('hough_dp', 'window', 1, 5, null_func)
     cv2.createTrackbar('hough_min_dist', 'window', 0, 50, null_func)
+    cv2.createTrackbar('hough_param1', 'window', 0, 50, null_func)
+    cv2.createTrackbar('hough_param2', 'window', 0, 50, null_func)
 
-    img = cv2.imread('dices.jpg')
+    base_img = cv2.imread('dices.jpg')
 
     while True:
+        img = base_img.copy()
+
         gray_bool       = cv2.getTrackbarPos('grayscale', 'window') == 1
         gaussian_bool   = cv2.getTrackbarPos('gaussian_toggle', 'window') == 1
-        kernel_g        = int(cv2.getTrackbarPos('kernel_gaussian', 'window'))
+        kernel_g        = cv2.getTrackbarPos('kernel_gaussian', 'window')
         sigma_g         = cv2.getTrackbarPos('sigma_gaussian', 'window')
         threshold_bool  = cv2.getTrackbarPos('threshold_toggle', 'window') == 1
         thresh          = cv2.getTrackbarPos('threshold', 'window')
@@ -42,25 +46,28 @@ def main():
         canny_thresh_2  = cv2.getTrackbarPos('canny_thresh_2', 'window')
         hough_bool      = cv2.getTrackbarPos('hough_toggle', 'window') == 1
         hough_dp        = cv2.getTrackbarPos('hough_dp', 'window')
-        hough_m_dist    = int(cv2.getTrackbarPos('hough_min_dist', 'window'))
+        hough_m_dist    = cv2.getTrackbarPos('hough_min_dist', 'window')
+        hough_param_1   = cv2.getTrackbarPos('hough_param1', 'window')
+        hough_param_2   = cv2.getTrackbarPos('hough_param2', 'window')
 
         if gray_bool:
             img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         
         if gaussian_bool:
-            img = cv2.GaussianBlur(img, (kernel_g, kernel_g), sigma_g)
+            if kernel_g % 2 == 1:
+                img = cv2.GaussianBlur(img, (kernel_g, kernel_g), sigma_g)
         
         if threshold_bool:
-            img = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)
+            _, img = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)
         
         if canny_bool:
             img = cv2.Canny(img, canny_thresh_1, canny_thresh_2)
-
-        # contours, hierarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         
         if hough_bool:
-            circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, hough_dp, hough_m_dist)
-            draw_circles(img, circles)
+            circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, hough_dp, hough_m_dist, hough_param_1, hough_param_2)
+            
+            if circles:
+                draw_circles(img, circles)
 
         cv2.imshow('Dices', img)
 
