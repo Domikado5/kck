afc4ac8f5a7f5f5a5539296bf100305f91c215f1
D3nz13 <seba.szczepan2@gmail.com>
2021-11-15 13:41:35 +0100

    Added contour finding

--- lab5 - images project/dice_recognition.py
+++ lab5 - images project/dice_recognition.py
@@ -24,16 +24,14 @@ def main():
     cv2.createTrackbar('canny_toggle', 'window', 0, 1, null_func)
     cv2.createTrackbar('canny_thresh_1', 'window', 0, 255, null_func)
     cv2.createTrackbar('canny_thresh_2', 'window', 0, 255, null_func)
-    cv2.createTrackbar('hough_toggle', 'window', 0, 1, null_func)
-    cv2.createTrackbar('hough_dp', 'window', 1, 5, null_func)
-    cv2.createTrackbar('hough_min_dist', 'window', 0, 50, null_func)
-    cv2.createTrackbar('hough_param1', 'window', 0, 50, null_func)
-    cv2.createTrackbar('hough_param2', 'window', 0, 50, null_func)
+    cv2.createTrackbar('contour_toggle', 'window', 0, 1, null_func)
+    cv2.createTrackbar('contour_area', 'window', 0, 1000, null_func)
 
     base_img = cv2.imread('dices.jpg')
 
     while True:
         img = base_img.copy()
+        img_2 = base_img.copy()
 
         gray_bool       = cv2.getTrackbarPos('grayscale', 'window') == 1
         gaussian_bool   = cv2.getTrackbarPos('gaussian_toggle', 'window') == 1
@@ -44,11 +42,8 @@ def main():
         canny_bool      = cv2.getTrackbarPos('canny_toggle', 'window') == 1
         canny_thresh_1  = cv2.getTrackbarPos('canny_thresh_1', 'window')
         canny_thresh_2  = cv2.getTrackbarPos('canny_thresh_2', 'window')
-        hough_bool      = cv2.getTrackbarPos('hough_toggle', 'window') == 1
-        hough_dp        = cv2.getTrackbarPos('hough_dp', 'window')
-        hough_m_dist    = cv2.getTrackbarPos('hough_min_dist', 'window')
-        hough_param_1   = cv2.getTrackbarPos('hough_param1', 'window')
-        hough_param_2   = cv2.getTrackbarPos('hough_param2', 'window')
+        contour_bool    = cv2.getTrackbarPos('contour_toggle', 'window') == 1
+        area            = cv2.getTrackbarPos('contour_area', 'window')
 
         if gray_bool:
             img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -63,13 +58,17 @@ def main():
         if canny_bool:
             img = cv2.Canny(img, canny_thresh_1, canny_thresh_2)
         
-        if hough_bool:
-            circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, hough_dp, hough_m_dist, hough_param_1, hough_param_2)
+        if contour_bool:
+            contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             
-            if circles:
-                draw_circles(img, circles)
+            for contour in contours:
+                if cv2.contourArea(contour) > area:
+                    (x, y, w, h) = cv2.boundingRect(contour)
+                    cv2.rectangle(img_2, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-        cv2.imshow('Dices', img)
+
+        cv2.imshow('Transformed_img', img)
+        cv2.imshow('Final_img', img_2)
 
         if cv2.waitKey(1) == 1: break
     
